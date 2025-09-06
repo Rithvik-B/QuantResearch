cc72a2599effc0ab2163ab5916d10c9f86dc72f8
Rithvik-B <rithvik.iitb@gmail.com>
2025-09-06 08:17:57 +0530

    img loading bug v2

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/quantresearch-landin-fl5k/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
